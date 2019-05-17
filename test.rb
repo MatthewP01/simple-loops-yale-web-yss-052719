cdc0f3c903a62f23c3cd0cd09a2d72133088aabd
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-17 17:19:59 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,11 @@
+
+def while_iterator(number_of_times)
+  # code your solution here using the "while" keyword
+  # hint: user a counter to tell the while loop when to stop!
+  counter = 0
+  while counter <= number_of_times
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
+end
+
+while_iterator(3)
