f6fe58bfda16d08b99590b2341995f4436572c24
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-17 17:28:41 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
 def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  for_counter
+  for_counter = 1..number_of_times
   for i in number_of_times
     puts "Welcome to Flatiron School's Web Development Course!"
   end
