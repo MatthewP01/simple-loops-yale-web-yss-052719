b2d8d897ce9cc17f9c11ef5c5640b3777156cdc3
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-17 17:26:23 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,9 +1,9 @@
-def until_iterator(number_of_times)
-  # code your solution here using the "until" keyword
-  # hint: use a counter to tell the until loop when to stop!
-  counter = 0
-  until counter == number_of_times
-    counter += 1
+def for_iterator(number_of_times)
+  # code your solution here using the "for" keyword
+  # remember that `for` requires a range of numbers. How can we turn our number into a range?
+  for i in number_of_times
     puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
+
+for_iterator(3)
